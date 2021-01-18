528974e8ef37f71bc9f7bf86d0bd22ca80c37f2a
Fabio Lopes <fabiolopes.dev@gmail.com>
2021-01-17 22:32:51 -0300

    Let's try to deploy again

--- server.ts
+++ server.ts
@@ -3,7 +3,7 @@ const { resolve } = require('path');
 const cors = require('cors');
 
 const app = express();
-app.use(cors);
+app.use(cors());
 app.use('/', express.static(resolve(__dirname, 'build')));
 
 app.listen(process.env.PORT || 3000);
